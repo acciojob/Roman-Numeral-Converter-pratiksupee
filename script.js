f52f310173129adc5752674d60999760a786057c
pratiksupee <135314162+pratiksupee@users.noreply.github.com>
2025-12-25 13:20:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 function convertToRoman(num) {
   if (num === 0) return "";
 
-  const obj = {
-    0: ['M', 1000],
-    1: ['D', 500],
-    2: ['C', 100],
-    3: ['L', 50],
-    4: ['X', 10],
-    5: ['V', 5],
-    6: ['I', 1]
-  };
+  const symbols = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
 
   let res = "";
 
-  for (let i = 0; i <= 6; i++) {
-    let symbol = obj[i][0];
-    let value = obj[i][1];
-
+  for (let i = 0; i < symbols.length; i++) {
+    const [symbol, value] = symbols[i];
     while (num >= value) {
       res += symbol;
       num -= value;
